40844a38fb60997280eff51fd0cb7cdb76e30aa6
YevhenSerdiuk2022 <jcserdyuk@gmail.com>
2022-12-24 21:08:03 +0200

    add search/spiner/and fix slider

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import './js/main-page/slider-film';
 import './js/main-page/gallery';
+import './js/main-page/search';
+import './js/main-page/spiner';
 import './js/my-library';
 import './js/closeModal';
 import {
--- src/js/main-page/slider-film.js
+++ src/js/main-page/slider-film.js
@@ -1,4 +1,5 @@
 import FilmotekaApi from '../api-service/filmoteka-api';
+import filmsCardSliderTpl from '../../templates/slider-films.hbs';
 
 const refs = {
   sliderContainer: document.querySelector('.slide-track'),
@@ -15,17 +16,9 @@ export function renderTrendy() {
     });
 }
 
-function renderSliderFilms(results) {
-  const markup = results
-    .map(result => {
-      return `
-    <div class="slider-element">
-        <img class="slider-image" src="https://image.tmdb.org/t/p/w500${result.poster_path}" alt="${result.title} "
-            data-id="${result.id}"
-            onerror="this.onerror=null;this.src='https://ik.imagekit.io/tc8jxffbcvf/default-movie-portrait_EmJUj9Tda5wa.jpg?tr=fo-auto,di-';" />
-    </div>
-    `;
-    })
-    .join('');
-  refs.sliderContainer.insertAdjacentHTML('beforeend', markup);
+
+
+function renderSliderFilms(articles) {
+  refs.sliderContainer.innerHTML = filmsCardSliderTpl(articles);
+ 
 }
