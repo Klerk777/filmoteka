770f82eccc71174e796e511af3fef668f23ab3f9
Volodymyr Bakumenko <Klerk.mail@gmail.com>
2022-12-26 01:45:46 +0200

    On branch fix/change-fun-in-pagination Your branch is up to date with 'origin/fix/change-fun-in-pagination'.
    Changes to be committed:
    	modified:   src/js/main-page/search.js

--- src/js/main-page/search.js
+++ src/js/main-page/search.js
@@ -26,20 +26,15 @@ const options = {
   lastItemClassName: 'tui-last-child',
   template: {
     page: '<a href="#" class="tui-page-btn">{{page}}</a>',
-    currentPage:
-      '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+    currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
     moveButton:
-      '<a href="#" class="tui-page-btn tui-{{type}}">' +
-      '<span class="tui-ico-{{type}}">{{type}}</span>' +
-      '</a>',
+      '<a href="#" class="tui-page-btn tui-{{type}}">' + '<span class="tui-ico-{{type}}">{{type}}</span>' + '</a>',
     disabledMoveButton:
       '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
       '<span class="tui-ico-{{type}}">{{type}}</span>' +
       '</span>',
     moreButton:
-      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
-      '<span class="tui-ico-ellip">...</span>' +
-      '</a>',
+      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' + '<span class="tui-ico-ellip">...</span>' + '</a>',
   },
 };
 
@@ -102,14 +97,14 @@ pagination.on('afterMove', function (eventData) {
 function paginationSearch() {
   if (filmotekaApi.query === '') {
     filmotekaApi
-      .fetchTrendFilm()
+      .fetchInTrendFilmWithGenres()
       .then(appendResultsMarkup)
       .catch(err => {
         refs.containerList.innerHTML = `<img class="catch-error-pagination" src="}"/>`;
       });
   } else {
     filmotekaApi
-      .fetchSearchFilm()
+      .fetchSearchFilmWithGenres()
       .then(appendResultsMarkup)
       .catch(err => {
         refs.containerList.innerHTML = `<img class="catch-error-pagination" src="}"/>`;
