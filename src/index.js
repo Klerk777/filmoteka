4fc442f051d771077c52678369b608e4ee9dc326
pdubovskyi <petro.dubovskyi@gmail.com>
2022-12-25 14:14:20 +0200

    footer-modal

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import './js/main-page/slider-film';
 import './js/main-page/gallery';
 import './js/my-library';
 import './js/closeModal';
-import  './js/modal-info/modalInfoFilm';
-
+import './js/modal-info/modalInfoFilm';
+import './js/modal-info/footer-modal';
