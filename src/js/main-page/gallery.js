feb22728ffc9a7fa112b71cee4004798199680c5
Volodymyr Bakumenko <Klerk.mail@gmail.com>
2022-12-25 19:16:16 +0200

    Update gallery.js

--- src/js/main-page/gallery.js
+++ src/js/main-page/gallery.js
@@ -2,18 +2,14 @@ import FilmotekaApi from '../api-service/filmoteka-api';
 import filmsCardTpl from '../../templates/card-films.hbs';
 
 
-
 const refs = {
    containerList: document.querySelector('.js-card'),
 }
 const filmotekaApi = new FilmotekaApi();
-// renderHomepage();
 
 export function renderHomepage() {
   filmotekaApi.resetPage();
-  filmotekaApi.fetchInTrendFilmWithGenres().then(appendResultsMarkup);
-  
-   
+  filmotekaApi.fetchInTrendFilmWithGenres().then(appendResultsMarkup);   
 }
 
 function appendResultsMarkup(results) {
