1563bcbc0c69eff69b888718ff819ae321863cfc
YevhenSerdiuk2022 <99655492+YevhenSerdiuk2022@users.noreply.github.com>
2022-12-26 18:31:33 +0200

    add resetPage

--- src/js/main-page/search.js
+++ src/js/main-page/search.js
@@ -70,6 +70,7 @@ function onSearch(evt) {
       renderHomepage();
       refs.containerWarningResults.textContent = `Sorry, there no results found. Try searching for something else! `;
     } else {
+      filmotekaApi.resetPage();
       pagination.setTotalItems(data.total_pages);
       pagination.setItemsPerPage(Math.ceil(data.total_pages / 20));
       pagination.reset();
