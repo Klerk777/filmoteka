5ca72ace486df8b8bd25a7e0d9ccf42d651c4af4
Volodymyr Bakumenko <Klerk.mail@gmail.com>
2022-12-26 18:00:13 +0200

    Your branch is up to date with 'origin/fix/footer-dark'.
    Changes to be committed:
    	modified:   src/js/theme-change.js
    	modified:   src/sass/layouts/_themeswitch.scss

--- src/js/theme-change.js
+++ src/js/theme-change.js
@@ -5,11 +5,9 @@ const LOCALSTORAGE_THEME_KEY = 'theme';
 toggleRef.addEventListener('change', event => {
   if (!bodyRef.classList.contains('dark-theme')) {
     bodyRef.classList.add('dark-theme');
-    footerDarktheme.classList.add('dark-theme');
     localStorage.setItem(LOCALSTORAGE_THEME_KEY, 'dark-theme');
   } else {
     bodyRef.classList.remove('dark-theme');
-    footerDarktheme.classList.remove('dark-theme');
     localStorage.removeItem(LOCALSTORAGE_THEME_KEY, 'dark-theme');
   }
 });
@@ -20,6 +18,5 @@ function setThemeFromLocalStorage() {
   if (localStorage.getItem(LOCALSTORAGE_THEME_KEY)) {
     bodyRef.classList.add('dark-theme');
   }
-  console.log('Theme is: '), bodyRef.classList;
 }
 setThemeFromLocalStorage();
